69ab2f1a4c8e52b5089985f2cab2709de0b38c3a
CarlosGonac <106280620+CarlosGonac@users.noreply.github.com>
2022-07-07 22:37:02 -0400

    Cambio de PC

---
+++ ejercicios.js
@@ -0,0 +1,9 @@
+function mayorMenorIgual (num1, num2) {
+    if 
+    if (num1 < num2) {
+        return num2 + " es mayor que " + num1
+    } else if (num1 > num2) {
+        return num1 + " es mayor que " + num2
+    }
+    return num1 + " es igual que " + num2
+}
